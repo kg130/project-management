67b7d7f6411ec63b18eff087c51d9f14c47251d9
Keshav Garg <keshavgarg@Keshavs-MacBook-Pro.local>
2021-08-24 12:08:25 +0530

    Changed port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: `${window.location.protocol}//${window.location.hostname}:8090`,
+  baseUrl: `${window.location.protocol}//${window.location.hostname}:8080`,
   networkName: 'prime',
   dbId: 'projects'
 };
